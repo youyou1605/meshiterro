858b02b52694afde7b3db902043a1c0a6ade18a9
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2022-12-09 13:54:02 +0000

    [Finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about',as: 'about'
+
+  resources :postimages, only: [:new,:index,:show]
+  get '/homes/about' => "homes#about",as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_081034) do
+ActiveRecord::Schema.define(version: 2022_12_08_095558) do
+
+  create_table "postimages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
