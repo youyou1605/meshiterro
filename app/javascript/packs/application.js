87ffe3d45f895d65c21cb7c318f114d8dd549c50
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2022-12-15 15:02:03 +0000

    [Finish]4-24

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,12 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+import "channels"
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
